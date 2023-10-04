d212ddb4a3eb3274fe33db4a5d5a2a4752e1f354
Joachim Spange <spange.joachim@gmail.com>
2023-10-04 13:58:49 +0200

    :tada: Fridge
    INO file to run at Arduino brain of the Fridge

---
+++ Arduino/Fridge/Fridge/Fridge.ino
@@ -0,0 +1,16 @@
+/*
+ * I have a gap. A gap between my fridge and my wall, currently I fill it with the storage of two drying racks
+ * Let's utelize it a bit better by building a Wine, vegetable and cheese cabinet.
+ * 
+ * Joachim Spange KodeWorks, house day 7th July 2023
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
