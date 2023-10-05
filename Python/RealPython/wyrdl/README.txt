d3402867f8d70e4e3ce6047918a7857af5912ff6
Joachim Spange <spange.joachim@gmail.com>
2023-10-05 10:03:53 +0200

    :truck: :white_check_mark: refactor and unit tests

--- Python/RealPython/wyrdl/README.txt
+++ Python/RealPython/wyrdl/README.txt
@@ -1,1 +1,4 @@
 https://realpython.com/lessons/python-wordle-clone-overview/
+
+DOCTEST with
+python3 -m doctest -v wyrdl.py
--- Python/RealPython/wyrdl/wyrdl.py
+++ Python/RealPython/wyrdl/wyrdl.py
@@ -1,25 +1,56 @@
 # wyrdl.py
 import pathlib
 import random
+from string import ascii_letters
 GUESSES = 6
-WORDLIST = pathlib.Path("wordlist.txt")
-word = random.choice([word.upper() 
-	for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")])
-for i in range(1, GUESSES+1):
-	guess = input(f"Guess {i}/{GUESSES}: ").upper()
-	if guess == word:
-		print("Correct")
-		break
-	print("Wrong")
-	print("Correct letters:")
-	c ={letter for letter, correct in zip(guess, word) if letter == correct}
-	print(c) if c else print("None!")
-	print("Misplaced letters:")
-	m =set(word) & set(guess) - c
-	print(m) if m else print("None!")
-	print("Wrong letters:")
-	w = set(guess) - set(word)
-	print(w) if w else print("None!")
+WORD_LENGTH = 5
 
-else:
-	print(f"Failed! Coeect word was {word}")
+
+def get_random_word(wordlist):
+	return random.choice([word.upper() 
+		for word in wordlist
+		if len(word) == WORD_LENGTH and all(letter in ascii_letters for letter in word)])
+
+def show_guess(guess, word):
+	"""Show the user's guess in the terminal and clarify all letters.
+
+	## Example:
+
+	>>> show_guess("CRANE", "SNAKE")
+	Correct letters: A, E
+	Misplaced letters: N
+	Wrong letters: C, R
+	"""
+
+
+	c ={letter for letter, correct in zip(guess, word) if letter == correct}
+	print("Correct letters:", ", ".join(sorted(c))) if c else print("No Correct letters")
+	m =set(word) & set(guess) - c
+	print("Misplaced letters:", ", ".join(sorted(m))) if m else print("No Misplaced letters")
+	w = set(guess) - set(word)
+	print("Wrong letters:", ", ".join(sorted(w))) if w else print("No Wrong letters!")
+
+
+def game_over(word):
+	print(f"Failed! Correct word was {word}")
+
+
+def main():
+	# Pre-process
+	word_path = pathlib.Path(__file__).parent / "wordlist.txt"
+	word = get_random_word(word_path.read_text(encoding="utf-8").split("\n"))
+
+	# Process (main loop)
+	for guess_num in range (1, GUESSES+1):
+		guess = input(f"\nGuess {guess_num}: ").upper()
+
+		show_guess(guess, word)
+		if guess == word:
+			break
+	
+	# Post-process
+	else:
+		game_over(word)
+
+if __name__ == '__main__':
+	main()
