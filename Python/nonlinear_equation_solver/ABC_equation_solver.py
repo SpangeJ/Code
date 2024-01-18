f845b92778aa87fd7d711af4b0977f045075ca10
Joachim Spange <spange.joachim@gmail.com>
2024-01-18 15:55:34 +0100

    🎨

--- Python/nonlinear_equation_solver/ABC_equation_solver.py
+++ Python/nonlinear_equation_solver/ABC_equation_solver.py
@@ -3,12 +3,16 @@ from scipy.optimize import fsolve
 # Solve A, B and C in A + Bx + Cx²
 def f(vars):
     A, B, C = vars
-    return [A + B*VALUE[0] + C*VALUE[0]**2 - RESULTS[0],
-            A + B*VALUE[1] + C*VALUE[1]**2 - RESULTS[1],
-            A + B*VALUE[2] + C*VALUE[2]**2 - RESULTS[2]]
+    return [A + B*X[0] + C*X[0]**2 - Y[0],
+            A + B*X[1] + C*X[1]**2 - Y[1],
+            A + B*X[2] + C*X[2]**2 - Y[2]]
 
-VALUE = [0, 5, 9]
-RESULTS = [0, -1905154, -7244856]
+X = [0, 5, 9]
+Y = [0, -1905154, -7244856]
 A, B, C = fsolve(f, (1,0,0))
-print(f"For the equation f(x) = A + Bx + CX², with f({VALUE[0]}) = {RESULTS[0]}, f({VALUE[1]}) = {RESULTS[1]}, f({VALUE[2]}) = {RESULTS[2]} the solution is:")
+print(f"For the equation f(x) = A + Bx + CX², with f({X[0]}) = {Y[0]}, f({X[1]}) = {Y[1]}, f({X[2]}) = {Y[2]} the solution is:")
 print(f"A: {A:.4f}, B:{B:.8f}, C:{C:.8f}")
+check = f([A, B, C])
+if abs(check[0]) > 0.1 or abs(check[1]) > 0.1 or abs(check[2]) > 0.1:
+    print("The solution is not matching!")
+    print(f"Control: f({X[0]})={check[0]+Y[0]:.2f}, f({X[1]})={check[1]+Y[1]:.2f}, f({X[2]})={check[2]+Y[2]:.2f}")
--- Python/nonlinear_equation_solver/nonlinear_equation_solver.py
+++ Python/nonlinear_equation_solver/nonlinear_equation_solver.py
@@ -1,15 +1,19 @@
+from math import exp
 from scipy.optimize import fsolve
 
-# Solve A and B in A + B^x
+# Solve A, B, C in A + B*e^(Cx)
 def f(vars):
-    A, B = vars
-    return [A + B**POWERS[0] - RESULTS[0],
-            A + B**POWERS[1] - RESULTS[1]]
+    A, B, C = vars
+    return [A + B*exp(C*X[0]) - Y[0],
+            A + B*exp(C*X[1]) - Y[1],
+            A + B*exp(C*X[2]) - Y[2]]
 
-POWERS = [0, 9]
-RESULTS = [6, 2]
-A, B = fsolve(f, (0,1))
-print(f"For the equation f(x) = A + B**x, with f({POWERS[0]}) = {RESULTS[0]}, and f({POWERS[1]}) = {RESULTS[1]}, the solution is:")
-print(f"A: {A:.4f}, B:{B:.4f}")
-check = f([A, B])
-print(f"Control: f({POWERS[0]})={check[0]+RESULTS[0]:.2f}, f({POWERS[1]})={check[1]+RESULTS[1]:.2f}")
+X = [0, 5, 9]
+Y = [0, -3, 5]
+A, B, C = fsolve(f, (0,1,1))
+print(f"For the equation f(x) = A + B*e**(Cx), with f({X[0]}) = {Y[0]}, f({X[1]}) = {Y[1]}, and f({X[2]}) = {Y[2]}, the solution is:")
+print(f"A: {A:.4f}, B:{B:.4f}, C:{C:.4f}")
+check = f([A, B, C])
+if abs(check[0]) > 0.1 or abs(check[1]) > 0.1 or abs(check[2]) > 0.1:
+    print("The solution is not matching!")
+    print(f"Control: f({X[0]})={check[0]+Y[0]:.2f}, f({X[1]})={check[1]+Y[1]:.2f}, f({X[2]})={check[2]+Y[2]:.2f}")
